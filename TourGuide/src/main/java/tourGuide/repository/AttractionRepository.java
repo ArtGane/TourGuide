63e7ad4983e1e18c1c03977367189280edc11742
ArtGane <argane.samson@gmail.com>
2023-06-20 09:42:32 +0200

    mauvaise initialisation de liste correction pour initialisation dans la méthode

--- TourGuide/src/main/java/tourGuide/repository/AttractionRepository.java
+++ TourGuide/src/main/java/tourGuide/repository/AttractionRepository.java
@@ -5,6 +5,7 @@ import gpsUtil.location.Attraction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -13,9 +14,10 @@ public class AttractionRepository {
     @Autowired
     GpsUtil gpsUtil = new GpsUtil();
 
-    private List<Attraction> attractions = gpsUtil.getAttractions();
+    private List<Attraction> attractions = new ArrayList<>();
 
     public List<Attraction> getAllAttractions() {
+        attractions = gpsUtil.getAttractions();
         return attractions;
     }
 
